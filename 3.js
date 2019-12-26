f42ccc11d4bd87230aeedcc56c78ca725b9b5ed8
Sapfir0 <sapfir999999@yandex.ru>
2019-12-26 21:24:22 +0400

    Написал тесты к легкой задаче

--- 3.js
+++ 3.js
@@ -1,7 +1,4 @@
 
-const array = [1, 1, 2, 3, 4, -51, 12, 12, 12, -51];
-
-
 function equalSequenceLength(array) {
     let sequenceLength = 0;
     for (let i = 1; i < array.length; i++) {
@@ -12,5 +9,33 @@ function equalSequenceLength(array) {
     return sequenceLength;
 }
 
-const a = equalSequenceLength(array);
-console.log(a);
+
+// зона тестов ниже)
+
+var assert = require('assert');
+
+function runTest(sequence, expected) {
+    const actual = equalSequenceLength(sequence);
+    assert.equal(actual, expected)
+    console.log("Sequence:", sequence, "; Actual: ", actual, "; Expected: ", expected) //не, это не отладка
+}
+
+class Test {
+    constructor(sequence, expectedResult) {
+        this.sequence = sequence
+        this.expectedResult = expectedResult
+    }
+}
+
+example = new Test([1, 1, 2, 3, 4, -51, 12, 12, 12, -51], 3)
+longSequenceOnStart = new Test([1, 1, 1, 1, 2, 3, 4, -51, 12, 12, 12, -51], 5)
+shortAndEqualsSequence = new Test([1, 1, 1], 2)
+empty = new Test([], 0)
+onlyOneElement = new Test([0], 0)
+
+var array = [example, longSequenceOnStart, shortAndEqualsSequence, empty, onlyOneElement]
+
+array.forEach(element => {
+    runTest(element.sequence, element.expectedResult)
+});
+
