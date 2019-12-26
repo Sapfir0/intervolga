2a9a4ebd8623f42b58a03f04ed7c0da654b44614
Sapfir0 <sapfir999999@yandex.ru>
2019-12-26 22:46:29 +0400

    Теперь png конвертируется с прозрачностью хорошо

--- 1.php
+++ 1.php
@@ -6,19 +6,22 @@
 <body>
 
 <?php 
-$imageFilename = "./image/image.png";
+$imageFilename = "./image/2.png";
 $desiredWidth = 200; // изображение сожмется до 200 по ширине, а по высоте пропорционально
 $oldImage = imagecreatefrompng($imageFilename);
 [$width, $height] = getimagesize($imageFilename);
 $compressionRatio = $width/$desiredWidth; 
 $newImageWidth = $width / $compressionRatio; 
 $newImageHeight = $height / $compressionRatio; 
+
 $newImage = imagecreatetruecolor($newImageWidth, $newImageHeight); 
-$newImageFilename  = "./image/image_small.png";
+imagesavealpha($newImage, true);
+imagefill($newImage, 0, 0, imagecolorallocatealpha($newImage, 0, 0, 0, 127));
 imagecopyresampled($newImage, $oldImage, 0, 0, 0, 0, $newImageWidth, $newImageHeight, $width, $height);
 
-$imageQuality = 100;
-imagejpeg($newImage, $newImageFilename, $imageQuality);
+$imageQuality = 0; // для джпега это должно быть равно 100
+$newImageFilename  = "./image/image_small.png";
+imagepng($newImage, $newImageFilename, $imageQuality);
 
 echo "<img height=100 width=200 src=$newImageFilename />"; 
 ?>
